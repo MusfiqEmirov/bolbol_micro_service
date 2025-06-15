368956b0fee777f231b2fa2ff301531e3426d0a4
Elvin <elvinhaxverdiyev777@gamil.com>
2025-06-15 20:28:21 +0400

    configure redis and celery

--- users_service/users_service/settings.py
+++ users_service/users_service/settings.py
@@ -148,3 +148,14 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     )
 }
+
+# Celery settings
+CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL')
+CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND')
+CELERY_IGNORE_RESULT = True
+CELERY_TIMEZONE = 'UTC'
+
+#Redis settings
+REDIS_HOST = os.getenv('REDIS_HOST')         
+REDIS_PORT = os.getenv('REDIS_PORT')
+REDIS_DB = os.getenv('REDIS_DB')
